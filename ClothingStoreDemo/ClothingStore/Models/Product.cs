e14cd33b6bb535436e79d7cd7ef59ad0f6eaa43e
KevinU20221275 <U20221275@univo.edu.sv>
2024-04-03 21:43:06 -0600

    added labels to properties for users view

--- ClothingStoreDemo/ClothingStore/Models/Product.cs
+++ ClothingStoreDemo/ClothingStore/Models/Product.cs
@@ -1,17 +1,24 @@
-﻿namespace ClothingStore.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClothingStore.Models
 {
     public class Product
     {
         public int Id { get; set; }
 
+        [Display(Name = "Nombre del Producto")]
         public string ProductName { get; set; }
 
+        [Display(Name = "Descripcion del Producto")]
         public string Description { get; set; }
 
+        [Display(Name = "Precio")]
         public decimal Price { get; set;}
 
+        [Display(Name = "Categoria")]
         public int CategoryId { get; set; }
 
+        [Display(Name = "Talla")]
         public int SizeId { get; set; }
 
         public Category? Category { get; set; }
