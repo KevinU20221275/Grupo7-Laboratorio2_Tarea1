61795f0ace23dba3dc833eef2e15ca57cfabdae8
KevinU20221275 <U20221275@univo.edu.sv>
2024-04-03 18:45:49 -0600

    finished product controller

--- ClothingStoreDemo/ClothingStore/Controllers/ProductController.cs
+++ ClothingStoreDemo/ClothingStore/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
-﻿using ClothingStore.Repositories.Products;
+﻿using ClothingStore.Models;
+using ClothingStore.Repositories.Products;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ClothingStore.Controllers
 {
@@ -8,9 +10,24 @@ namespace ClothingStore.Controllers
     {
         private readonly IProductRepository _productRepository;
 
+        private SelectList _categoriesLits;
+        private SelectList _sizesList;
+
         public ProductController(IProductRepository productRepository)
         {
             _productRepository = productRepository;
+
+            _categoriesLits = new SelectList(
+                    _productRepository.GetAllCategories(),
+                    nameof(Category.Id),
+                    nameof(Category.CategoryName)
+                );
+
+            _sizesList = new SelectList(
+                    _productRepository.GetAllSizes(),
+                    nameof(Size.Id),
+                    nameof(Size.SizeDescription)
+                );
         }
         // GET: ProductController
         public ActionResult Index()
@@ -18,72 +35,111 @@ namespace ClothingStore.Controllers
             return View(_productRepository.GetAll());
         }
 
-        // GET: ProductController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
 
         // GET: ProductController/Create
         public ActionResult Create()
         {
+            ViewBag.Categories = _categoriesLits;
+            ViewBag.Sizes = _sizesList;
+
             return View();
         }
 
         // POST: ProductController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(Product product)
         {
             try
             {
+                _productRepository.Add(product);
+
+                TempData["message"] = "Datos Guardados con exito";
+
                 return RedirectToAction(nameof(Index));
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                TempData["message"] = ex.Message; 
+
+                ViewBag.Categories = _categoriesLits;
+                ViewBag.Sizes = _sizesList;
+
+                return View(product);
             }
         }
 
         // GET: ProductController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            var product = _productRepository.GetById(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.Categories = _categoriesLits;
+            ViewBag.Sizes = _sizesList;
+
+            return View(product);
         }
 
         // POST: ProductController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(Product product)
         {
             try
             {
+                _productRepository.Edit(product);
+
+                TempData["message"] = "Datos Editados con exito";
+
                 return RedirectToAction(nameof(Index));
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                TempData["message"] = ex.Message;
+
+                ViewBag.Categories = _categoriesLits;
+                ViewBag.Sizes = _sizesList;
+
+                return View(product);
             }
         }
 
         // GET: ProductController/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            var product = _productRepository.GetById(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
         }
 
         // POST: ProductController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(Product product)
         {
             try
             {
+                _productRepository.Delete(product.Id);
+
+                TempData["message"] = "Datos eliminados con exito";
+
                 return RedirectToAction(nameof(Index));
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                TempData["message"] = ex.Message;
+
+                return View(product);
             }
         }
     }
