da3fd690018f2869db6bbdabd0265f2f23e6bf6e
KevinU20221275 <U20221275@univo.edu.sv>
2024-04-04 23:25:41 -0600

    The interface and methods for sales were created

---
+++ ClothingStoreDemo/ClothingStore/Repositories/Sales/ISaleRepository.cs
@@ -0,0 +1,16 @@
+﻿using ClothingStore.Models;
+
+namespace ClothingStore.Repositories.Sales
+{
+    public interface ISaleRepository
+    {
+        void Add(Sale sale);
+        void Delete(int id);
+        void Edit(Sale sale);
+        IEnumerable<Sale> GetAll();
+        Sale? GetById(int id);
+        IEnumerable<Customer> GetAllCustomers();
+        IEnumerable<Employee> GetAllEmployees();
+        IEnumerable<Product> GetAllProducts();
+    }
+}
---
+++ ClothingStoreDemo/ClothingStore/Repositories/Sales/SaleRepository.cs
@@ -0,0 +1,125 @@
+﻿using ClothingStore.Data;
+using ClothingStore.Models;
+using Dapper;
+using System.Data;
+
+namespace ClothingStore.Repositories.Sales
+{
+    public class SaleRepository : ISaleRepository
+    {
+        private readonly ISqlDataAccess _dataAccess;
+
+        public SaleRepository(ISqlDataAccess dataAccess)
+        {
+            _dataAccess = dataAccess;
+        }
+
+        public IEnumerable<Customer> GetAllCustomers()
+        {
+            string query = "SELECT Id, CustomerFirstName + ' ' + CustomerLastName AS CustomerName FROM Customer";
+
+            using (var connection = _dataAccess.GetConnection())
+            {
+                return connection.Query<Customer>(query);
+            }
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            string query = "SELECT Id, EmployeeFirstName + ' ' + EmployeeLastName AS EmployeeName FROM Employee";
+
+            using (var connection = _dataAccess.GetConnection())
+            {
+                return connection.Query<Employee>(query);
+            }
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            string query = "SELECT Id, ProductName + '      $' + CAST(Price AS VARCHAR(10)) AS ProductInfo FROM Product;";
+
+            using (var connection = _dataAccess.GetConnection())
+            {
+                return connection.Query<Product>(query);
+            }
+        }
+
+       
+        public IEnumerable<Sale> GetAll()
+        {
+            using (var connection = _dataAccess.GetConnection())
+            {
+                string storedProcedure = "spSale_GetAll";
+
+                var sales = connection.Query<Sale, Customer, Employee, Product, Sale>(storedProcedure, (sale,customer,employee,product) =>
+                {
+                    sale.Customer = customer;
+                    sale.Employee = employee;
+                    sale.Product = product;
+
+                    return sale;
+                },
+                splitOn: "CustomerName,EmployeeName,ProductName",
+                commandType: CommandType.StoredProcedure);
+
+                return sales;
+            }
+        }
+
+        public Sale? GetById(int id)
+        {
+            using (var connection = _dataAccess.GetConnection())
+            {
+                string storedProcedure = "spSale_GetById";
+
+                return connection.QueryFirstOrDefault<Sale>(
+                            storedProcedure,
+                            new { Id = id },
+                            commandType: CommandType.StoredProcedure
+                    );
+            }
+        }
+
+        public void Add(Sale sale)
+        {
+            using (var connection = _dataAccess.GetConnection())
+            {
+                string storedProcedure = "spSale_Insert";
+
+                connection.Execute(
+                        storedProcedure,
+                        new { sale.CustomerId, sale.EmployeeId, sale.ProductId, sale.Quantity, sale.SaleDate },
+                        commandType: CommandType.StoredProcedure
+                    );
+            }
+        }
+
+        public void Edit(Sale sale)
+        {
+            using (var connection = _dataAccess.GetConnection())
+            {
+                string storedProcedure = "spSale_Update";
+
+                connection.Execute(
+                        storedProcedure,
+                        new { sale.Id, sale.CustomerId, sale.EmployeeId, sale.ProductId, sale.Quantity, sale.SaleDate },
+                        commandType: CommandType.StoredProcedure
+                    );
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var connection = _dataAccess.GetConnection())
+            {
+                string storedProcedure = "spSale_Delete";
+
+                connection.Execute(
+                        storedProcedure,
+                        new { Id = id },
+                        commandType: CommandType.StoredProcedure
+                    );
+            }
+        }
+    }
+}
