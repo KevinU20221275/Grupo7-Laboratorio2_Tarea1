f28cc6a21dcf57d667131732bee5c266b023d8ad
KevinU20221275 <U20221275@univo.edu.sv>
2024-04-05 17:35:32 -0600

    The interface and methods for size were created

---
+++ ClothingStoreDemo/ClothingStore/Repositories/Sizes/ISizeRepository.cs
@@ -0,0 +1,13 @@
+﻿using ClothingStore.Models;
+
+namespace ClothingStore.Repositories.Sizes
+{
+    public interface ISizeRepository
+    {
+        void Add(Size size);
+        void Delete(int id);
+        void Edit(Size size);
+        IEnumerable<Size> GetAll();
+        Size? GetById(int id);
+    }
+}
---
+++ ClothingStoreDemo/ClothingStore/Repositories/Sizes/SizeRepository.cs
@@ -0,0 +1,87 @@
+﻿using ClothingStore.Data;
+using ClothingStore.Models;
+using Dapper;
+using System.Data;
+
+namespace ClothingStore.Repositories.Sizes
+{
+    public class SizeRepository : ISizeRepository
+    {
+        private readonly ISqlDataAccess _dataAccess;
+
+        public SizeRepository(ISqlDataAccess dataAccess)
+        {
+            _dataAccess = dataAccess;
+        }
+
+        public IEnumerable<Size> GetAll()
+        {
+            using (var connection = _dataAccess.GetConnection())
+            {
+                string storeProcedure = "spSize_GetAll";
+
+                return
+                    connection.Query<Size>(
+                        storeProcedure,
+                        commandType: CommandType.StoredProcedure
+                    );
+            }
+        }
+
+        public Size? GetById(int id)
+        {
+            using (var connection = _dataAccess.GetConnection())
+            {
+                string storedProcedure = "spSize_GetById";
+
+                return connection.QueryFirstOrDefault<Size>(
+                            storedProcedure,
+                            new { Id = id },
+                            commandType: CommandType.StoredProcedure
+                    );
+            }
+        }
+
+        public void Add(Size size)
+        {
+            using (var connection = _dataAccess.GetConnection())
+            {
+                string storedProcedure = "spSize_Insert";
+
+                connection.Execute(
+                        storedProcedure,
+                        new { size.SizeDescription },
+                        commandType: CommandType.StoredProcedure
+                    );
+            }
+        }
+
+        public void Edit(Size size)
+        {
+            using (var connection = _dataAccess.GetConnection())
+            {
+                string storedProcedure = "spSize_Update";
+
+                connection.Execute(
+                        storedProcedure,
+                        new { size.Id, size.SizeDescription },
+                        commandType: CommandType.StoredProcedure
+                    );
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var connection = _dataAccess.GetConnection())
+            {
+                string storedProcedure = "spSize_Delete";
+
+                connection.Execute(
+                        storedProcedure,
+                        new { Id = id },
+                        commandType: CommandType.StoredProcedure
+                    );
+            }
+        }
+    }
+}
