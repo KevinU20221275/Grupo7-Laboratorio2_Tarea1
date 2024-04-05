9bb41caa89d3f75ef64e5ef15b47c9d35003aa58
keoto02 <guerrerodavid42069@gmail.com>
2024-04-05 12:01:15 -0600

    Feat: Controller for Category complete

---
+++ ClothingStoreDemo/ClothingStore/Controllers/CategoryController.cs
@@ -0,0 +1,124 @@
+﻿using ClothingStore.Models;
+using ClothingStore.Repositories.Categories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClothingStore.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        // GET: CategoryController
+        public ActionResult Index()
+        {
+            return View(_categoryRepository.GetAll());
+        }
+
+        // GET: CategoryController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CategoryController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CategoryController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Category category)
+        {
+            try
+            {
+                _categoryRepository.Add(category);
+
+                TempData["addCategory"] = "Datos Guardados con éxito";
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                TempData["message"] = ex.Message;
+
+                return View(category);
+            }
+        }
+
+        // GET: CategoryController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var category = _categoryRepository.GetById(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // POST: CategoryController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Category category)
+        {
+            try
+            {
+                _categoryRepository.Edit(category);
+
+                TempData["editCategory"] = "Datos Editados con éxito";
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                TempData["message"] = ex.Message;
+
+                return View(category);
+            }
+        }
+
+        // GET: CategoryController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var category = _categoryRepository.GetById(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // POST: CategoryController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Category category)
+        {
+            try
+            {
+                _categoryRepository.Delete(category.Id);
+
+                TempData["deleteCategory"] = "Datos Eliminados con éxito";
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                TempData["message"] = ex.Message;
+
+                return View();
+            }
+        }
+    }
+}
